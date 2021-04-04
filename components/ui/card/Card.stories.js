e00c9b99d851730db205e03f8ba437d17b85e159
Krutie Patel <krutie.patel@gmail.com>
2021-04-05 01:04:24 +1000

    Activate controls with args

--- components/ui/card/Card.stories.js
+++ components/ui/card/Card.stories.js
@@ -14,7 +14,7 @@ export default {
 }
 
 // Define template for Primary Story
-const PrimaryTemplate = () => ({
+const PrimaryTemplate = (args) => ({
   components: { Card, CardHeader, CardFooter, CardImage },
   template: `
   <card
