a19cbb9074fdf722ff34f5f625f54cfbe0666cee
Krutie Patel <krutie.patel@gmail.com>
2021-04-05 06:37:19 +1000

    Display Code Blocks

--- components/ui/card/Card.stories.js
+++ components/ui/card/Card.stories.js
@@ -23,39 +23,39 @@ import readme from "./docs/readme.md";
 
 // Describe card component
 export default {
-	title: "JS/Card",
-	component: Card,
-	args: primaryConfig.args,
-	argTypes: primaryConfig.argTypes,
-	decorators: [
-		() => ({
-			template:
-				'<div style="display: flex; align-items: center; justify-content: center; height: 100vh;"><story /></div>',
-		}),
-	],
-	parameters: {
-		docs: {
-			description: {
-				component: readme,
-			},
-		},
-	},
+  title: "JS/Card",
+  component: Card,
+  args: primaryConfig.args,
+  argTypes: primaryConfig.argTypes,
+  decorators: [
+    () => ({
+      template:
+        '<div style="display: flex; align-items: center; justify-content: center; height: 100vh;"><story /></div>',
+    }),
+  ],
+  parameters: {
+    docs: {
+      description: {
+        component: readme,
+      },
+    },
+  },
 };
 
 // Define template for Primary Story
 const PrimaryTemplate = (args, { argTypes }) => ({
-	components: { Card, CardHeader, CardFooter, CardImage },
-	props: Object.keys(argTypes),
-	template: `
+  components: { Card, CardHeader, CardFooter, CardImage },
+  props: Object.keys(argTypes),
+  template: `
   <card
     :primary-color="primaryColor"
     :padding="padding"
     :border-width="borderWidth"
     :border-radius="borderRadius"
     :card-bg="cardBg"
-		class="w-72"
+    class="w-72"
   >
-	  <card-header>
+    <card-header>
       <span> Tailwind </span>
       <span> Masonry Grid </span>
     </card-header>
@@ -66,7 +66,7 @@ const PrimaryTemplate = (args, { argTypes }) => ({
     </card-header>
     <card-footer
       :invert="invert"
-			:name="name"
+      :name="name"
       category="Collection of card designs"
     >
     </card-footer>
@@ -76,24 +76,33 @@ const PrimaryTemplate = (args, { argTypes }) => ({
 
 // named export Primary to create respective story
 export const Primary = PrimaryTemplate.bind({});
+// add source code for individual story
+// parameters at story level
+Primary.parameters = {
+  docs: {
+    source: {
+      code: "`<Card> ... </Card>`",
+    },
+  },
+};
 
 // SVG
 // Define template for SVG Story
 const SVGTemplate = (args, { argTypes }) => ({
-	components: { Card, CardContent, CardFooter },
-	props: Object.keys(argTypes),
-	template: `
+  components: { Card, CardContent, CardFooter },
+  props: Object.keys(argTypes),
+  template: `
   <card
     :padding="padding"
-		:primary-color="primaryColor"
-		:secondary-color="secondaryColor"
-		:border-width="borderWidth"
-		:border-radius="borderRadius"
-		:card-bg="cardBg"
-		class="w-72"
+    :primary-color="primaryColor"
+    :secondary-color="secondaryColor"
+    :border-width="borderWidth"
+    :border-radius="borderRadius"
+    :card-bg="cardBg"
+    class="w-72"
   >
     <card-content>
-			<components :is="svgComponent" :color="primaryColor" />
+      <components :is="svgComponent" :color="primaryColor" />
     </card-content>
     <card-footer
       :invert="invert"
@@ -112,24 +121,24 @@ SVG.argTypes = svgConfig.argTypes;
 // Gradient
 // Define template for SVG Story
 const GradientTemplate = (args, { argTypes }) => ({
-	components: { Card, CardContent, CardFooter, PageSubtitle },
-	props: Object.keys(argTypes),
-	template: `
-	<card
+  components: { Card, CardContent, CardFooter, PageSubtitle },
+  props: Object.keys(argTypes),
+  template: `
+  <card
     :padding="padding"
-		:primary-color="primaryColor"
-		:secondary-color="secondaryColor"
-		:border-width="borderWidth"
-		:border-radius="borderRadius"
-		:card-bg="cardBg"
-		class="w-72"
+    :primary-color="primaryColor"
+    :secondary-color="secondaryColor"
+    :border-width="borderWidth"
+    :border-radius="borderRadius"
+    :card-bg="cardBg"
+    class="w-72"
   >
     <card-content :gradient="gradient">
       <page-subtitle class="uppercase font-title" :title="name">
       </page-subtitle>
     </card-content>
     <card-footer
-			:invert="invert"
+      :invert="invert"
       class="text-gray-800"
       category="Collection of card designs"
     >
